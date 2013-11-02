d8c3a64ea1c5ef217c2c27f0c29ef139f304f6a2
songzx <360898142@qq.com>
2013-11-02 14:34:18 +0800

    提交

--- water/example/src/main/java/com/water/example/data2view/Template.java
+++ water/example/src/main/java/com/water/example/data2view/Template.java
@@ -3,8 +3,7 @@ package com.water.example.data2view;
 import java.io.InputStream;
 
 /**
- * 处理表单片段与整块jsp模块
- * 合成jsp 根据对象属性值，正则表达式判断输入合法性，表单类型，JMS的主题选择
+ * 处理表单片段与整块jsp模块 合成jsp 根据对象属性值，正则表达式判断输入合法性，表单类型，JMS的主题选择
  * 
  * @author 0000
  * @date Nov 2, 2013
@@ -13,18 +12,19 @@ import java.io.InputStream;
 public abstract class Template {
 	private Script script = null;
 	private Authz authz = null;
-	
+
 	/**
 	 * 填充模板，并输出
+	 * 
 	 * @return
 	 */
-	public InputStream fillin(){
+	public InputStream fillin() {
 		script.check();
 		authz.check();
 		rule();
 		return null;
 	}
-	
+
 	/**
 	 * 处理各种填充形式
 	 */
